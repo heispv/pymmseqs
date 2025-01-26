b5ea594b23a486fe2687a24c8f3b834e6214540a
heispv <peymanvahidi1998@gmail.com>
2025-01-26 16:57:07 +0100

    Making some minor updates

--- examples/search/search_ex.py
+++ examples/search/search_ex.py
@@ -2,6 +2,6 @@
 
 from pymseqs.commands import createdb, search
 
-createdb('target.fasta', 'pydb/t_db', verbosity=3)
-createdb('query.fasta', 'pydb/q_db', verbosity=3)
-search ('pydb/q_db', 'pydb/t_db', 'pyout/res', tmp_dir='tmp_python', verbosity=3)
+createdb("target.fasta", "pydb/t_db", v=3)
+createdb("query.fasta", "pydb/q_db", v=3)
+search ("pydb/q_db", "pydb/t_db", "pyout/res", tmp_dir="tmp_python", v=3)
