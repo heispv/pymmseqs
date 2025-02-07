da9184ce14693f68c827a792dec24a4f2854a3eb
heispv <peymanvahidi1998@gmail.com>
2025-02-07 16:36:07 +0100

    Adding new easy_linsearch func

--- pymseqs/commands/__init__.py
+++ pymseqs/commands/__init__.py
@@ -2,10 +2,12 @@ from .createdb import createdb
 from .search import search
 from .cluster import cluster
 from .easy_search import easy_search
+from .easy_linsearch import easy_linsearch
 
 __all__ = [
     "createdb",
     "search",
     "cluster",
-    "easy_search"
+    "easy_search",
+    "easy_linsearch",
 ]
