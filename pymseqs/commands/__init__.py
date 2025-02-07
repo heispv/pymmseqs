5705899562ae108e322ecb165d7d323d1aaa124d
heispv <peymanvahidi1998@gmail.com>
2025-02-07 12:50:30 +0100

    Minor update and adding new func

--- pymseqs/commands/__init__.py
+++ pymseqs/commands/__init__.py
@@ -1,9 +1,11 @@
 from .createdb import createdb
 from .search import search
 from .cluster import cluster
+from .easy_search import easy_search
 
 __all__ = [
-    'createdb',
-    'search',
-    'cluster'
+    "createdb",
+    "search",
+    "cluster",
+    "easy_search"
 ]
