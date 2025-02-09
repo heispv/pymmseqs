39470892eb0e7616f9a4a4b2e8c7f8bc3afe56a3
heispv <peymanvahidi1998@gmail.com>
2025-02-09 17:20:26 +0100

    Modifying based on new createtaxdb func

--- examples/easy_taxonomy/easy_taxonomy_ex.py
+++ examples/easy_taxonomy/easy_taxonomy_ex.py
@@ -1,9 +1,8 @@
 # examples/easy_taxonomy/easy_taxonomy_ex.py
 
-from pymseqs.commands import createdb, easy_taxonomy
+from pymseqs.commands import createdb, createtaxdb, easy_taxonomy
 
-createdb("target.fasta", "pydb/t_db")
-createdb("query.fasta", "pydb/q_db")
-# Right now we don't have the createtaxdb command, so it doens't work
+createdb("target.fasta", "pydb/t_db", tmp_dir="tmp")
+createdb("query.fasta", "pydb/q_db", tmp_dir="tmp")
 createtaxdb("pydb/t_db", tmp_dir="tmp")
 easy_taxonomy("pydb/q_db", "pydb/t_db", "pyout/taxonomy_res", tmp_dir="tmp")
