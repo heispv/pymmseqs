649a22ee2067461da57752cff81f68507ea9b758
heispv <peymanvahidi1998@gmail.com>
2025-02-09 11:04:06 +0100

    Updating the example, for better output handling

--- examples/cluster/cluster_ex.py
+++ examples/cluster/cluster_ex.py
@@ -3,13 +3,13 @@
 from pymseqs.commands import createdb
 from pymseqs.commands import cluster
 
-createdb("input.fasta", "output/db")
+createdb("input.fasta", "output/db_output/db")
 
 cluster(
-    "output/db",
-    "output/clusters",
-    "output/tmp",
-    min_seq_id=1)
+    "output/db_output/db",
+    "output/cluster_output/clusters",
+    "output/tmp"
+)
 
 # TODO: Then you should also use createtsv to create a TSV file from the clusters (human readable format)
 # createtsv("output/db", "output/clusters", "output/clusters.tsv")
