f0d559edb2aeefa630bee4ad3343de4ee40a5c65
heispv <peymanvahidi1998@gmail.com>
2025-02-11 15:19:27 +0100

    Supproting also .yaml config input

--- examples/createdb/createdb_ex.py
+++ examples/createdb/createdb_ex.py
@@ -1,5 +1,16 @@
 # examples/createdb/createdb_ex.py
 
+from pymseqs.config import CreateDBConfig
 from pymseqs.commands import createdb
 
-createdb("input.fasta", "output/db", write_lookup=False, v=3)
+# Create and validate config
+config = CreateDBConfig(
+    input_fasta="input.fasta",
+    db_name="output/db"
+)
+
+# To use a config file, uncomment the following line
+
+# config = CreateDBConfig.from_yaml("config.yaml")
+
+createdb(config)
