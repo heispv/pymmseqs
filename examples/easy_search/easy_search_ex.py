8a4cbb3c970be96b3debfe7c60a515df793d9d40
heispv <peymanvahidi1998@gmail.com>
2025-02-18 15:13:20 +0100

    Adding config for easy_search

--- examples/easy_search/easy_search_ex.py
+++ examples/easy_search/easy_search_ex.py
@@ -1,11 +1,13 @@
 # examples/easy_search/easy_search_ex.py
 
-from pymseqs.commands import easy_search
+from pymmseqs.config import EasySearchConfig
 
-easy_search (
+easy_search = EasySearchConfig(
     query_fasta="query.fasta",
     target_fasta_or_db="target.fasta",
     alignment_file="output/result",
     tmp_dir="output/tmp_python",
-    format_mode=4
+    format_mode=4,
     )
+
+easy_search.run()
