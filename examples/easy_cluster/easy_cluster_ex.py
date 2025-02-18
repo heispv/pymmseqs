b8ec6139d077e0bb33ada8cdaee0bb1e262e1c73
heispv <peymanvahidi1998@gmail.com>
2025-02-18 20:25:03 +0100

    Adding new wrapper func, with example, and yaml

--- examples/easy_cluster/easy_cluster_ex.py
+++ examples/easy_cluster/easy_cluster_ex.py
@@ -1,10 +1,12 @@
 # examples/easy_cluster/easy_cluster_ex.py
 
-from pymseqs.commands import easy_cluster
+from pymmseqs.config import EasyClusterConfig
 
-easy_cluster(
-    "input.fasta",
-    "output/clusters",
-    "output/tmp",
-    v=3,
+easy_cluster = EasyClusterConfig(
+    fasta_files="uniprot.fasta",
+    cluster_prefix="new_output/clusters",
+    tmp_dir="new_output/tmp",
+    v=2
 )
+
+easy_cluster.run()
