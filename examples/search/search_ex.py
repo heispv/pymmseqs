84bdb05c7cd645e7118d4e7d74991cdc2196a9a7
heispv <peymanvahidi1998@gmail.com>
2025-02-19 14:53:44 +0100

    Adding new config wrapper, yaml file, example for it

--- examples/search/search_ex.py
+++ examples/search/search_ex.py
@@ -1,7 +1,34 @@
 # examples/createdb/createdb_ex.py
 
-from pymseqs.commands import createdb, search
+from pymmseqs.config import (
+    CreateDBConfig,
+    SearchConfig
+)
 
-createdb("target.fasta", "pydb/t_db", v=3)
-createdb("query.fasta", "pydb/q_db", v=3)
-search ("pydb/q_db", "pydb/t_db", "pyout/res", tmp_dir="tmp_python", v=3)
+# Create createdb config objects
+query_db = CreateDBConfig(
+    fasta_file="query.fasta",
+    sequence_db="pydb/q_db",
+    v=3
+)
+target_db = CreateDBConfig(
+    fasta_file="target.fasta",
+    sequence_db="pydb/t_db",
+    v=3
+)
+
+# Run the config objects
+query_db.run()
+target_db.run()
+
+# Create search config object
+search_config = SearchConfig(
+    query_db="pydb/q_db",
+    target_db="pydb/t_db",
+    alignment_db="pyout/res",
+    tmp_dir="tmp_python",
+    v=3
+)
+
+# Run the search config object
+search_config.run()
--- pymmseqs/config/__init__.py
+++ pymmseqs/config/__init__.py
@@ -3,13 +3,14 @@
 from .base import BaseConfig
 from .createdb_config import CreateDBConfig
 from .createtaxdb_config import CreateTaxDBConfig
+from .search_config import SearchConfig
 from .easy_search_config import EasySearchConfig
 from .easy_cluster_config import EasyClusterConfig
-
 __all__ = [
     'BaseConfig',
     'CreateDBConfig',
     'CreateTaxDBConfig',
+    'SearchConfig',
     'EasySearchConfig',
-    'EasyClusterConfig'
+    'EasyClusterConfig',
 ]
