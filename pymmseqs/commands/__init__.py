aac596228704b2e4580c5c299b24d1dd0660c80e
heispv <peymanvahidi1998@gmail.com>
2025-02-24 16:27:07 +0100

    Removing old files

--- pymmseqs/commands/__init__.py
+++ pymmseqs/commands/__init__.py
@@ -1,19 +1,11 @@
 # pymmseqs/commands/__init__.py
 
-from .search import search
 from .cluster import cluster
-from .easy_search import easy_search
-from .easy_linsearch import easy_linsearch
-from .easy_cluster import easy_cluster
 from .easy_linclust import easy_linclust
 from .easy_taxonomy import easy_taxonomy
 from .createtaxdb import createtaxdb
 __all__ = [
-    "search",
     "cluster",
-    "easy_search",
-    "easy_linsearch",
-    "easy_cluster",
     "easy_linclust",
     "easy_taxonomy",
     "createtaxdb",
