2401b73690cd857ab2e78055bd32f5ae7cada8de
heispv <peymanvahidi1998@gmail.com>
2025-02-28 16:52:02 +0100

    Adding easy_cluster tools

--- pymmseqs/tools/__init__.py
+++ pymmseqs/tools/__init__.py
@@ -1,3 +1,13 @@
-from .evalue_histogram import plot_evalues
+from .evalue_histogram import (
+    plot_evalues,
+    plot_cluster_sizes
+)
+from .cluster_analysis import (
+    analyze_mmseqs_clusters
+)
 
-__all__ = ['plot_evalues']
+__all__ = [
+    'plot_evalues',
+    'plot_cluster_sizes',
+    'analyze_mmseqs_clusters'
+]
