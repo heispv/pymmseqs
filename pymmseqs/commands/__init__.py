9322b99e6cc4aea68aa7b296f3d3854bca636188
heispv <peymanvahidi1998@gmail.com>
2025-02-28 17:03:40 +0100

    Adding the createdb command with correct parsing

--- pymmseqs/commands/__init__.py
+++ pymmseqs/commands/__init__.py
@@ -1,9 +1,11 @@
 # pymmseqs/commands/__init__.py
 
+from .createdb import createdb
 from .easy_search import easy_search
 from .easy_cluster import easy_cluster
 
 __all__ = [
+    "createdb",
     "easy_search",
     "easy_cluster"
 ]
