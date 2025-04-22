6f64ae8286da1e3fd6664a5de4fa289cd80e0e5a
heispv <peymanvahidi1998@gmail.com>
2025-04-22 17:13:08 +0200

    bug: enhance EasyClusterParser to support customizable seq_id extraction from fasta headers - if seq_id extraction faild, only returns the header

--- pymmseqs/parsers/easy_cluster_parser.py
+++ pymmseqs/parsers/easy_cluster_parser.py
@@ -12,8 +12,28 @@ class EasyClusterParser:
     """
     A class for parsing the output of the EasyClusterConfig.
     """
-    def __init__(self, config: EasyClusterConfig):
+    def __init__(
+        self,
+        config: EasyClusterConfig,
+        seq_id_separator: str = "|",
+        seq_id_index: int = 1
+    ):
+        """
+        Parameters
+        ----------
+        config: EasyClusterConfig
+            The configuration object for the EasyCluster command.
+        seq_id_separator: str, optional
+            The separator used in the FASTA headers to separate the sequence ID from other information.
+            Default is "|".
+        seq_id_index: int, optional
+            The index of the sequence ID in the FASTA header.
+            Default is 1.
+        Note: It tries to extract the seq_id from the header using the separator and index, if it fails, it doesn't add the seq_id to the member.
+        """
         self.cluster_prefix = config.cluster_prefix
+        self.seq_id_separator = seq_id_separator
+        self.seq_id_index = seq_id_index
     
     def to_list(self) -> list:
         """
@@ -27,6 +47,7 @@ class EasyClusterParser:
             - "rep": The representative sequence ID.
             - "members": List of member dictionaries in the cluster with the following keys:
                 - "seq_id": Unique sequence identifier extracted from the header.
+                    - If the header has format like ">seq_id|header", the seq_id is extracted from the header.
                 - "header": Full FASTA header for the sequence.
                 - "sequence": Nucleotide or protein sequence.
 
@@ -40,7 +61,7 @@ class EasyClusterParser:
             "rep": rep,
             "members": members
             }
-            for rep, members in parse_fasta_clusters(f"{self.cluster_prefix}_all_seqs.fasta")
+            for rep, members in parse_fasta_clusters(f"{self.cluster_prefix}_all_seqs.fasta", self.seq_id_separator, self.seq_id_index)
         ]
     
     def to_pandas(self) -> pd.DataFrame:
@@ -49,12 +70,20 @@ class EasyClusterParser:
         for cluster in clusters:
             rep = cluster["rep"]
             for member in cluster["members"]:
-                rows.append({
-                    "rep": rep,
-                    "seq_id": member["seq_id"],
-                    "header": member["header"],
-                    "sequence": member["sequence"]
-                })
+                # If the member has a seq_id, add it to the rows
+                if "seq_id" in member:
+                    rows.append({
+                        "rep": rep,
+                        "seq_id": member["seq_id"],
+                        "header": member["header"],
+                        "sequence": member["sequence"]
+                    })
+                else:
+                    rows.append({
+                        "rep": rep,
+                        "header": member["header"],
+                        "sequence": member["sequence"]
+                    })
         return pd.DataFrame(rows).set_index('rep')
     
     def to_gen(self) -> Generator:
@@ -77,7 +106,7 @@ class EasyClusterParser:
         - When implementing streaming pipelines that process one cluster at a time.
         - When you need a dictionary format but want to avoid loading the entire dataset into memory.
         """
-        for rep, members in parse_fasta_clusters(f"{self.cluster_prefix}_all_seqs.fasta"):
+        for rep, members in parse_fasta_clusters(f"{self.cluster_prefix}_all_seqs.fasta", self.seq_id_separator, self.seq_id_index):
             yield {
                 "rep": rep,
                 "members": members
